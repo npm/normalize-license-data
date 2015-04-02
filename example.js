0ec9c4e15257f2ffddf7c40108030b969ab53d27
Zeke Sikelianos <zeke@sikelianos.com>
2015-04-01 21:02:08 -0700

    clean up example script

--- example.js
+++ example.js
@@ -1,25 +1,25 @@
 var normalize = require('./')
 
 normalize(null)
-// -> null
+// => null
 
 normalize('')
-// -> null
+// => null
 
 normalize('MIT')
-// - > {name: 'MIT', url: 'http://opensource.org/licenses/MIT'}
+// => {name: 'MIT', url: 'http://opensource.org/licenses/MIT'}
 
 normalize('BSD')
-// - > {name: 'BSD', url: 'http://opensource.org/licenses/BSD-2-Clause'}
+// => {name: 'BSD', url: 'http://opensource.org/licenses/BSD-2-Clause'}
 
 normalize('unfamiliar')
-// - > {name: 'unfamiliar'}
+// => {name: 'unfamiliar'}
 
 normalize({name: 'wtfpl', url: 'https:///wtfpl.net'})
-// - > {name: 'wtfpl', url: 'https:///wtfpl.net'}
+// => {name: 'wtfpl', url: 'https:///wtfpl.net'}
 
 normalize({type: 'wtfpl', url: 'https:///wtfpl.net'})
-// - > {name: 'wtfpl', url: 'https:///wtfpl.net'}
+// => {name: 'wtfpl', url: 'https:///wtfpl.net'}
 
 normalize('https://custom-license.com')
-// - > {name: 'custom-license.com', url: 'https:///custom-license.com'}
+// => {name: 'custom-license.com', url: 'https:///custom-license.com'}
